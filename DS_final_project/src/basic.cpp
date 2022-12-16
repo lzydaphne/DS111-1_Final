6dd2d381433bc96c801a9f2f9a68e28a4c0ff134
lzydaphne <52618403+lzydaphne@users.noreply.github.com>
2022-12-16 08:02:44 +0800

    Update basic.cpp

--- DS_final_project/src/basic.cpp
+++ DS_final_project/src/basic.cpp
@@ -102,10 +102,10 @@ void basic(string selectedCase)
             // 已經有紀錄了
             if (!read_data.shortest_record[tuser_start_station])
             {
+                // 回傳single source 的dist array
                 read_data.shortest_record[tuser_start_station] = basic_graph.dijkstra(tuser_start_station, tuser_end_station);
                 int idx = 0;
 
-                cout << "test" << endl;
                 while (read_data.shortest_record[tuser_start_station][idx])
                 {
                     cout << "dij: " << read_data.shortest_record[tuser_start_station][idx] << endl;
@@ -115,7 +115,7 @@ void basic(string selectedCase)
                 // read_data.shortest_record[tuser_start_station] = basic_graph.Graph::dist_graph;
             }
             shortest_path = read_data.shortest_record[tuser_start_station][tuser_end_station];
-            cout << " shortest_path " << shortest_path << endl;
+            cout << "  shortest_path " << shortest_path << endl;
 
             target.returned_time += shortest_path;
 
@@ -125,14 +125,22 @@ void basic(string selectedCase)
 
             // cout << "this station doesn't have bike_type" << endl;
 
-            if ((target.returned_time > tstart_time) || (tstart_time + shortest_path > tend_time))
+            // 直接拒絕
+            if (tstart_time + shortest_path > tend_time)
+            {
+                find = 0;
+                break;
+            }
+            // v1把不符合條件的都集合起來，直到找出符合兩個條件的target
+            // v2後來發現不行，因為第二個條件跟bike無關，是時間和最短距離的問題
+            if ((target.returned_time > tstart_time))
             {
                 // todo 待優化，這邊先以最保守的方式來拿記憶體空間
                 // 目的是，為了要暫時儲存不符合time資格的max node
                 int tmp_idx = 0;
                 BMNode *store_BMNode = new BMNode[bike_max_num];
 
-                while (find && (target.returned_time > tstart_time) || (tstart_time + shortest_path > tend_time))
+                while (find && (target.returned_time > tstart_time))
                 {
                     cout << "test3" << endl;
                     if (target.rental_price < 0)
@@ -141,25 +149,19 @@ void basic(string selectedCase)
                         break;
                     }
                     store_BMNode[tmp_idx++] = target;
-                    target = basic_stations[tuser_start_station][i].extractMax();
+                    target = basic_stations[tuser_start_station][tAC_bike_type[i]].extractMax();
                 }
 
                 cout << "test4" << endl;
                 // 再一一insert回去
                 for (int j = 0; j < tmp_idx; j++)
-                    basic_stations[tuser_start_station][i].insertKey(store_BMNode[j]);
+                    basic_stations[tuser_start_station][tAC_bike_type[i]].insertKey(store_BMNode[j]);
                 delete[] store_BMNode;
 
                 tmp_idx = 0; // 歸零
                 if (!find)
                     continue;
             }
-
-            /* if (tstart_time + shortest_path > tend_time)
-             {
-                 find = 0;
-                 continue;
-             }*/
 
             //* 比較，不同bike_type之間最高的rental_price
 
@@ -175,9 +177,9 @@ void basic(string selectedCase)
             else if (i != 0 && target.rental_price > compare.rental_price)
             {
                 //* 把前面已經拿到，但發現rental_price比較小的bike放回去
-                basic_stations[tuser_start_station][i].insertKey(compare);
+                basic_stations[tuser_start_station][tAC_bike_type[i]].insertKey(compare);
                 BMNode *ptr = &compare;
-                *ptr = target; // 把compare指向target
+                *ptr = target; // 把原本儲存compare的地址指向target
             }
         }
 
