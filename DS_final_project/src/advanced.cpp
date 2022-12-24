91bcfeb6fdab00a788e6f54bd6ef66629585e302
lzydaphne <52618403+lzydaphne@users.noreply.github.com>
2022-12-24 15:55:49 +0800

    Update advanced.cpp

--- DS_final_project/src/advanced.cpp
+++ DS_final_project/src/advanced.cpp
@@ -480,6 +480,7 @@ void advanced(string selectedCase)
             while (!wait_list.isEmpty())
             {
                 BMNode tmp = wait_list.extractMax();
+                cout << "put back wait node id" << tmp.id << endl;
                 basic_stations[tuser_start_station][stoi(tmp.bike_type)].insertKey(tmp);
             }
             //--------------
